8da69829d9433d32af9590c6907b8c6f0c88e928
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-09-15 19:43:00 -0700

    Add docstrings to preprocess_images.py functions
    Included comprehensive docstrings for functions in preprocess_images.py to enhance code readability and maintainability. These docstrings provide details on the purpose, parameters, and expected behavior of each function.

--- project/utils/preprocess_images.py
+++ project/utils/preprocess_images.py
@@ -1,3 +1,16 @@
+"""
+Module for preprocessing images.
+
+This module includes functions to resize and find images, as well as a main function
+to process images as a whole.
+
+Functions:
+    - resize_image: Resizes an image to given dimensions.
+    - find_images: Identifies and retrieves images from a specified directory.
+    - process_images: Handles the overall processing workflow for images.
+    - main: Entry point for the module, coordinating the complete image
+    preprocessing pipeline.
+"""
 import argparse
 import logging
 import time
@@ -18,6 +31,14 @@ logging.basicConfig(
 
 
 def resize_image(image_path, output_path, size):
+    """
+    Resize an image.
+    @param image_path: The file path of the input image to be resized.
+    @param output_path: The file path where the resized image will be saved.
+    @param size: The dimensions to resize the image to, specified as
+    a tuple (width, height).
+    @return: None
+    """
     try:
         with Image.open(image_path) as img:
             resized_img = img.resize(size,  Image.Resampling.LANCZOS)
@@ -29,6 +50,12 @@ def resize_image(image_path, output_path, size):
 
 
 def find_images(input_dir):
+    """
+    Recursively find images to resize.
+    @param input_dir: Directory to search for image files.
+    @return: A tuple containing a list of image paths and a dictionary
+    with the count of each image type.
+    """
     image_extensions = (".png", ".jpg", ".jpeg", ".bmp", ".tiff")
     image_count_by_type = defaultdict(int)
     image_paths = []
@@ -42,6 +69,14 @@ def find_images(input_dir):
 
 
 def process_images(input_dir, output_dir, size, max_workers):
+    """
+    This method does the work of resizing images.
+    @param input_dir: Directory path where input images are stored
+    @param output_dir: Directory path where resized images will be stored
+    @param size: Desired size (width, height) of the resized images
+    @param max_workers: Maximum number of worker processes to use for resizing
+    @return: None
+    """
     images, image_count_by_type = find_images(input_dir)
 
     if not images:
