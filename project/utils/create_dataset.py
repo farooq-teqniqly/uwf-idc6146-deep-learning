4dd24e9883011748e68c4619cfb7045e331accb8
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-09-16 15:09:04 -0700

    Refactor dataset saving functionality and add CLI support
    Replaced `imagenet.pkl` with `imagenet_original_res.pkl` to improve naming consistency. Refactored dataset saving code into a dedicated `save_dataset` function and added command-line interface (CLI) support using argparse. This refactoring enhances code modularity and usability for different input directories and output file paths.

--- project/utils/create_dataset.py
+++ project/utils/create_dataset.py
@@ -11,27 +11,41 @@ Classes and functions included:
 - `load_dataset`: Loads a dataset from a pickle file and returns the images,
 labels, and filenames.
 """
+import argparse
 import pickle
 
 import tensorflow as tf
 
-images_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
-generator = images_generator.flow_from_directory(r"C:\Temp\imagenet")
-train_images, train_labels = next(generator)
-filenames = generator.filenames
-batch_indices = generator.index_array[:len(train_images)]
-batch_filenames = [filenames[idx] for idx in batch_indices]
-batch_image_files = {batch_filenames[i]: train_images[i]
-                     for i in range(len(train_images))}
+def save_dataset(input_dir, output_file):
+    """
+    Saves a dataset into a file.
 
-data_to_save = dict(
-    train_images=train_images,
-    train_labels=train_labels,
-    batch_image_files=batch_image_files,
-)
+    This function processes image data from the specified input directory, normalizes the images, and saves them along with their labels and filenames into a specified output file using pickle.
 
-with open("imagenet.pkl", "wb") as file:
-    pickle.dump(data_to_save, file)
+    Args:
+        input_dir (str): The path to the directory containing the images.
+        output_file (str): The path where the processed dataset will be saved.
+
+    Raises:
+        Exception: An error occurred while processing the images or saving the data.
+    """
+    images_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
+    generator = images_generator.flow_from_directory(input_dir)
+    train_images, train_labels = next(generator)
+    filenames = generator.filenames
+    batch_indices = generator.index_array[:len(train_images)]
+    batch_filenames = [filenames[idx] for idx in batch_indices]
+    batch_image_files = {batch_filenames[i]: train_images[i]
+                         for i in range(len(train_images))}
+
+    data_to_save = dict(
+        train_images=train_images,
+        train_labels=train_labels,
+        batch_image_files=batch_image_files,
+    )
+
+    with open(output_file, "wb") as file:
+        pickle.dump(data_to_save, file)
 
 def load_dataset():
     """
@@ -45,3 +59,20 @@ def load_dataset():
     with open("dataset.pkl", "rb") as file:
         data = pickle.load(file)
     return data["train_images"], data["train_labels"], data["batch_image_files"]
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Pickle a dataset representing the ImageNet images.")
+
+    parser.add_argument("--input-dir", type=str, required=True,
+                        help="Path to the input folder containing images")
+
+    parser.add_argument("--output-file", type=str, required=True,
+                        help="Path to the output pickle file")
+
+    args = parser.parse_args()
+    save_dataset(args.input_dir, args.output_file)
+
+if __name__ == "__main__":
+    main()
+
