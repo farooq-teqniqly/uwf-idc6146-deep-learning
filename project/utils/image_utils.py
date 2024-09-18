9b9b69ea813b0670785860120dd4952de38ba3ba
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-09-18 15:46:47 -0700

    Refactor folder creation logic in image_utils.py
    Modularize folder creation by introducing two helper functions: `_create_output_folder` and `_create_image_class_folder`. Update unit tests to verify the existence of the necessary folders for training, validation, and testing within the specified directories.

--- project/utils/image_utils.py
+++ project/utils/image_utils.py
@@ -1,20 +1,36 @@
 import os.path
 from pathlib import Path
 
+from tensorboard.summary.v1 import image
+
+
 def create_train_test_validation_sets(
         input_dir:Path,
         output_dir:Path,
         filter,
         train_pct:float) -> None:
 
-    set_folder_paths = dict(
-        train=os.path.join(output_dir, "train"),
-        val=os.path.join(output_dir, "val"),
-        test=os.path.join(output_dir, "test")
-    )
+    output_folders = [_create_output_folder(output_dir, f) for f
+                      in ["train", "test", "val"]]
 
-    for folder_path in set_folder_paths.values():
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+    image_class_folders = [_create_image_class_folder(of, image_class)
+                           for of in output_folders
+                           for image_class in os.listdir(input_dir)]
 
-    files = input_dir.rglob(filter)
+
+
+def _create_output_folder(root_path:Path, folder_name:str) -> Path:
+    output_path = os.path.join(root_path, folder_name)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    return output_path
+
+def _create_image_class_folder(root_path:Path, image_class:str) -> Path:
+    image_class_path = os.path.join(root_path, image_class)
+
+    if not os.path.exists(image_class_path):
+        os.makedirs(image_class_path)
+
+    return image_class_path
--- tests/test_image_utils.py
+++ tests/test_image_utils.py
@@ -15,6 +15,10 @@ class TestImageUtils(unittest.TestCase):
     def test_create_train_test_validation_sets(self):
         input_dir = Path("./images224")
         output_dir = Path("test_create_train_test_validation_sets")
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
         filter = "*.jpeg"
 
         create_train_test_validation_sets(
@@ -23,9 +27,16 @@ class TestImageUtils(unittest.TestCase):
             filter,
             0.7)
 
-        train_folder = Path(os.path.join(output_dir, "train"))
-        files = list(train_folder.rglob(filter))
-        # assert len(files) == 14
+        train_folder = Path(os.path.join(output_dir, "train", "bird"))
+        val_folder = Path(os.path.join(output_dir, "val", "bird"))
+        test_folder = Path(os.path.join(output_dir, "test", "bird"))
+
+        assert os.path.exists(train_folder)
+        assert os.path.exists(val_folder)
+        assert os.path.exists(test_folder)
+
+        # train_file_count = list(train_folder.rglob(filter))
+        # assert len(train_file_count) == 14
 
 
 
