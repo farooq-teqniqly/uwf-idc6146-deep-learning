64b7d7f82f719745c02994488c1f6b7a756943b9
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-09-19 11:40:04 -0700

    Refactor test with parameterized.expand
    Refactor `test_create_train_test_validation_sets` to use `parameterized.expand` for testing multiple image classes. Updated assertions to dynamically check expected file counts for training, testing, and validation sets. Added helper method `_get_file_count` to streamline folder file count logic.

--- tests/test_image_utils.py
+++ tests/test_image_utils.py
@@ -3,6 +3,8 @@ import shutil
 import unittest
 from pathlib import Path
 
+from parameterized import parameterized
+
 from project.utils.image_utils import create_train_test_validation_sets
 
 TRAIN_FILE_COUNT = 14
@@ -23,27 +25,30 @@ class TestImageUtils(unittest.TestCase):
         if not os.path.exists(self._output_dir):
             os.makedirs(self._output_dir)
 
-    def test_create_train_test_validation_sets(self):
+    @parameterized.expand([
+        ("bird", 14, 4, 2),
+        ("cat", 14, 4, 2),
+    ])
+    def test_create_train_test_validation_sets(
+            self,
+            image_class,
+            expected_train_file_count,
+            expected_test_file_count,
+            expected_validation_file_count):
         self.create_output_dir_if_not_exists()
 
         create_train_test_validation_sets(self._input_dir, self._output_dir)
 
-        train_folder = Path(os.path.join(self._output_dir, "train", "bird"))
-        test_folder = Path(os.path.join(self._output_dir, "test", "bird"))
-        val_folder = Path(os.path.join(self._output_dir, "val", "bird"))
+        train_folder = Path(os.path.join(self._output_dir, "train", image_class))
+        test_folder = Path(os.path.join(self._output_dir, "test", image_class))
+        val_folder = Path(os.path.join(self._output_dir, "val", image_class))
 
-        assert os.path.exists(train_folder)
-        assert os.path.exists(val_folder)
-        assert os.path.exists(test_folder)
+        assert self._get_file_count(train_folder) == expected_train_file_count
+        assert self._get_file_count(test_folder) == expected_test_file_count
+        assert self._get_file_count(val_folder) == expected_validation_file_count
 
-        train_file_count = list(train_folder.rglob(JPEG_FILTER))
-        assert len(train_file_count) == TRAIN_FILE_COUNT
-
-        test_file_count = list(test_folder.rglob(JPEG_FILTER))
-        assert len(test_file_count) == TEST_FILE_COUNT
-
-        val_file_count = list(val_folder.rglob(JPEG_FILTER))
-        assert len(val_file_count) == VAL_FILE_COUNT
+    def _get_file_count(self, folder:Path) -> int:
+        return len(list(folder.rglob(JPEG_FILTER)))
 
 if __name__ == "__main__":
     unittest.main()
