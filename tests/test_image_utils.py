001a630971b41accc846532c7589011d8b93a802
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-09-18 18:13:11 -0700

    Refactor test setup and teardown in test_image_utils.py
    Updated `setUp` and `tearDown` methods to initialize and clean test directories. This ensures test files are handled consistently and cleanup is properly executed after tests run.

--- tests/test_image_utils.py
+++ tests/test_image_utils.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import shutil
 
 from project.utils.image_utils import create_train_test_validation_sets
 from pathlib import Path
@@ -7,30 +8,28 @@ from pathlib import Path
 class TestImageUtils(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self._input_dir = Path("./images224")
+        self._output_dir = Path("test_create_train_test_validation_sets")
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self._output_dir)
 
     def test_create_train_test_validation_sets(self):
-        input_dir = Path("./images224")
-        output_dir = Path("test_create_train_test_validation_sets")
-
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+        if not os.path.exists(self._output_dir):
+            os.makedirs(self._output_dir)
 
         filter = "*.jpeg"
 
         create_train_test_validation_sets(
-            input_dir,
-            output_dir,
+            self._input_dir,
+            self._output_dir,
             filter,
             0.7,
         0.2)
 
-        train_folder = Path(os.path.join(output_dir, "train", "bird"))
-        test_folder = Path(os.path.join(output_dir, "test", "bird"))
-        val_folder = Path(os.path.join(output_dir, "val", "bird"))
+        train_folder = Path(os.path.join(self._output_dir, "train", "bird"))
+        test_folder = Path(os.path.join(self._output_dir, "test", "bird"))
+        val_folder = Path(os.path.join(self._output_dir, "val", "bird"))
 
         assert os.path.exists(train_folder)
         assert os.path.exists(val_folder)
